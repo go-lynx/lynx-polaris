352236b4f94e9badea4af4f2d40afa1e7a2cdcdd
tan-zhuo <tanzhuo@aliyun.com>
2026-06-07 16:04:19 +0900

    chore: bump pluginVersion to v1.6.3

--- polaris.go
+++ polaris.go
@@ -25,7 +25,7 @@ const (
 	pluginName = "polaris.control.plane"
 
 	// pluginVersion represents the current version of the Polaris control plane plugin.
-	pluginVersion = "v1.6.1"
+	pluginVersion = "v1.6.3"
 
 	// pluginDescription briefly describes the functionality of the Polaris control plane plugin.
 	pluginDescription = "polaris control plane plugin for lynx framework"
